891f538a12b04fb93e92bbec73f14c772f1a506e
Nathan Matsuo <nathan@ucr-secure-21-10-13-135-27.wnet.ucr.edu>
2025-02-03 16:45:47 -0800

    added gScore function to test for A*, this is linear not manhhattened, not fully tested

--- test.py
+++ test.py
@@ -1,12 +1,32 @@
-board = [["1", "2", "3",], 
-         ["4", "5", "0"]]
-row = 1
-col = 2
+goalBoard = [[1, 2, 3], [4, 5, 6], [7, 8, 0]] #goal state 123456780
+#board = [[1, 2, 3], [4, 5, 0], [7, 8, 6]] #test baord 
+board = [[0, 1, 2], [3, 4, 5], [6, 7, 8]] #test baord 
 directions = [[1, 0],
                       [-1, 0],
                       [0, 1],
                       [0, -1]]
-delimiter = ""
-temp = delimiter.join(board[0])
 
-print(temp)
+
+def gScore(board):
+    goalBoard = [[1, 2, 3], [4, 5, 6], [7, 8, 0]] #goal state 123456780
+    score = 0
+    row = 0
+    col = 0
+    for r in range(3):
+        for c in range(3):
+
+            if board[r][c] == goalBoard[row][col]:
+                print("score:", score, row, r, col, c)
+                score += abs((row-r) + (col-c))
+                if col >= 2:
+                    col = 0
+                    row += 1
+                else:
+                    col += 1
+                print("incremetn row and col", row, col)
+                print()
+    print("final score", score)
+    return
+
+
+gScore(board)
